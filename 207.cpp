5a1b59e7b61edd21107ec491131d8af10cb3605b
colig <2396033578@qq.com>
2023-09-16 23:40:21 +0800

    2023/09/16 周六

---
+++ 207.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for (size_t i = 0; i < numCourses; i++) {
+            
+            
+        }
+
+    }
+};
---
+++ 2428.cpp
@@ -0,0 +1,32 @@
+/*
+ * my best dev env
+ * os: windows ?  
+ *
+ * good: 
+ *  embedded softwares rely on windows 
+ *  we can play windows hack 
+ *  neovim is fine 
+ *  bunch of IDEs but vscode is just fine 
+ *  it's stable
+ *
+ * bad: 
+ *  always don't known softwares do what
+ *  cmd is trush - terminal.exe could fix this?
+ *  dir not sensitive 
+ *  (neovim no plugins but vscode is fine) 
+ *  (sorts of projects need unix-like env, we can use vscode remote), this results:
+ *  -- overhead
+ *  -- inconsistence -> avoid use multi dev env or use remote->overhead
+ *
+ *  wsl is trash. we could avoid inconsistence and just use local) exists) 
+ *  we will play game to waste my time
+ *
+ */
+
+#include <vector>
+using namespace std;
+
+vector<int> test;
+class Solution {
+public:
+};
